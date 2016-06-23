3aaa2efb78a150dc55dd8f033a9e42208cb83c94
Muhammet Ali Bekiroglu <muhammetalibekiroglu@maleeb.local>
2016-06-23 15:20:30 +0200

    + test

--- iot-frontend/lib/public/react/Sensor/NewSensor.jsx
+++ iot-frontend/lib/public/react/Sensor/NewSensor.jsx
@@ -10,7 +10,7 @@ var NewSensor = React.createClass({
 
   getInitialState: function() {
     return {
-      isActive: true,
+      isActive: true, // test
       name: '',
       sensorType: '',
       attachedGateway: '',
